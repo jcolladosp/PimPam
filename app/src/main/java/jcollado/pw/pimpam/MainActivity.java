885daf8e0d48d4df1a8ea572beda3726138f7458
Jose Collado <jocolsan@inf.upv.es>
2017-03-08 09:27:32 +0100

    added drawer

--- app/src/main/java/jcollado/pw/pimpam/MainActivity.java
+++ app/src/main/java/jcollado/pw/pimpam/MainActivity.java
@@ -2,12 +2,68 @@ package jcollado.pw.pimpam;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.mikepenz.materialdrawer.AccountHeader;
+import com.mikepenz.materialdrawer.AccountHeaderBuilder;
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.DividerDrawerItem;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IProfile;
 
 public class MainActivity extends AppCompatActivity {
+    private Drawer result = null;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        setNavigationDrawer();
+
+    }
+
+    private void setNavigationDrawer(){
+
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+
+
+        // Create the AccountHeader
+        AccountHeader headerResult = new AccountHeaderBuilder()
+                .withActivity(this)
+                .withHeaderBackground(R.drawable.header)
+                .withOnlyMainProfileImageVisible(true)
+                .addProfiles(
+                        new ProfileDrawerItem().withName("Andres Paul").withEmail("pimpam@gmail.com").withIcon(getResources().getDrawable(R.drawable.profile))
+                )
+
+                .build();
+        PrimaryDrawerItem item1 = new PrimaryDrawerItem().withIdentifier(1).withName("Añadir comic");
+        SecondaryDrawerItem item2 = new SecondaryDrawerItem().withIdentifier(2).withName("Ver colección");
+
+        //create the drawer and remember the `Drawer` result object
+        result = new DrawerBuilder()
+                .withActivity(this)
+                .withToolbar(toolbar)
+                .withAccountHeader(headerResult)
+                .addDrawerItems(item1,item2)
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+                        result.closeDrawer();
+                        return true;
+                    }
+                })
+                .build();
+
     }
 }
