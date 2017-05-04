6128bbed1c637f93204d273b9a211a685452711e
Jose Collado <jocolsan@inf.upv.es>
2017-05-04 12:12:20 +0200

    codigo de barra del pimpam funcionando

--- app/src/main/java/jcollado/pw/pimpam/controller/Barcode_Fragment.java
+++ app/src/main/java/jcollado/pw/pimpam/controller/Barcode_Fragment.java
@@ -1,13 +1,9 @@
 package jcollado.pw.pimpam.controller;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,6 +14,7 @@ import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
 import butterknife.BindView;
+import butterknife.OnClick;
 import jcollado.pw.pimpam.utils.BaseFragment;
 
 import butterknife.ButterKnife;
@@ -45,41 +42,6 @@ public class Barcode_Fragment extends BaseFragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getActivity().setContentView(R.layout.fragment_barcode_);
-        buttonScan = (Button) getActivity().findViewById(R.id.buttonScan);
-
-
-        buttonScan.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                IntentIntegrator integrator = new IntentIntegrator(getActivity());
-                integrator.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES);
-                integrator.setPrompt("Scan");
-                integrator.setCameraId(0);
-                integrator.setBeepEnabled(false);
-                integrator.setBarcodeImageEnabled(false);
-                integrator.initiateScan();
-            }
-        });
-
-
-
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        IntentResult result = IntentIntegrator.parseActivityResult(requestCode,resultCode,data);
-        if (result!= null) {
-            if (result.getContents() == null){
-                Log.d("MainActivity", "Cancel Scan");
-                Toast.makeText(getActivity(), "Cancelled", Toast.LENGTH_LONG).show();
-            }else {
-                Log.d("MainActivity","Scanned");
-                Toast.makeText(getActivity(),"Scanned" + result.getContents(), Toast.LENGTH_LONG).show();
-            }
-        } else {
-            super.onActivityResult(requestCode, resultCode, data);
-        }
 
     }
 
@@ -87,13 +49,35 @@ public class Barcode_Fragment extends BaseFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_barcode_, container, false);
+        View view = inflater.inflate(R.layout.fragment_barcode, container, false);
         ButterKnife.bind(this, view);
         //((AppCompatActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         //((AppCompatActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
 
         return view;
     }
+    @OnClick(R.id.buttonScan)
+    void onScan()
+    {
+        new IntentIntegrator(getActivity()).initiateScan(); // `this` is the current Activity
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null) {
+            if(result.getContents() == null) {
+                Toast.makeText(getActivity(), "Cancelled", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(getActivity(), "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+
 
 
 
